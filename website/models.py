e53b51866b103fb185fea37802aa9910a61cf2dd
LinZoldyck <hermann.soulton-agboton@hotmail.com>
2019-07-01 10:11:25 +0200

    Update blog application template +  application template

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ fs=FileSystemStorage(location='/media')
 class Formation(models.Model):
     name = models.CharField(max_length= 500)
     start_date = models.DecimalField(max_digits = 6, decimal_places=0)
-    status = models.CharField(max_length= 7)
+    status = models.CharField(max_length= 8)
     school = models.CharField(max_length=500)
     photo = models.ImageField(upload_to="images/")
 
--- website/views.py
+++ website/views.py
@@ -4,8 +4,11 @@ from . models import Formation
 # Create your views here.
 
 def index(request):
-   Formations = Formation.objects.order_by('status')
+   Formations = Formation.objects.order_by('start_date')
    return  render(request, 'website/index.html',{'Formations': Formations})
  
 def contact(request):
     return render(request, 'website/contact.html')
+
+def about(request):
+   return render (request, 'website/about.html')
