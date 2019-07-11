a29304827b264fbb10a9fd87884617a70b2037a1
hermann <hermann.soulton-agboton@hotmail.com>
2019-07-11 22:47:51 +0200

    Ready to heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'website.apps.WebsiteConfig',
+    
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Extra lookup directories for collectstatic to find static files
+
+django_heroku.settings(locals())
